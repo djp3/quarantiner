fcc982c5c97558907ade4bd4dd84451cc8624215
Don Patterson <d_j_p_3@djp3.net>
2016-09-02 10:39:38 -0700

    Adding another process I found

--- Adobe/cleanUpAdobe.sh
+++ Adobe/cleanUpAdobe.sh
@@ -22,7 +22,6 @@ sleep 1
 
 echo "Hard Kill"
 for i in `ps auxwww | grep -i "com.adobe.ARMDC" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
-sleep 1
 echo "*** end "
 echo "**************"
 
@@ -35,70 +34,117 @@ for i in `ps auxwww | grep -i "AGSService" | grep -v grep | tr -s " " | cut -d' 
 sleep 1
 echo "Hard Kill"
 for i in `ps auxwww | grep -i "AGSService" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
-sleep 1
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing AdobeGCClient"
 echo "*** begin "
-for i in `ps auxwww | grep -i "AdobeGCClient.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "AdobeGCClient.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "AdobeGCClient.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing CS5 Service Manager"
 echo "*** begin "
-for i in `ps auxwww | grep -i "CS5ServiceManager.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "CS5ServiceManager.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "CS5ServiceManager.app" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing Adobe Desktop Service"
 echo "*** begin "
-for i in `ps auxwww | grep -i "Adobe Desktop Service" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Adobe Desktop Service" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Adobe Desktop Service" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing Core Sync"
 echo "*** begin "
-for i in `ps auxwww | grep -i "Core Sync" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Core Sync" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Core Sync" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing AdobeIPC"
 echo "*** begin "
-for i in `ps auxwww | grep -i "AdobeIPC" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "AdobeIPC" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "AdobeIPC" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing Adobe Crash Reporter"
 echo "*** begin "
-for i in `ps auxwww | grep -i "Adobe Crash Reporter" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Adobe Crash Reporter" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Adobe Crash Reporter" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing Adobe javascript"
 echo "*** begin "
-for i in `ps auxwww | grep -i "Adobe Creative Cloud.*js$" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Adobe Creative Cloud.*js$" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Adobe Creative Cloud.*js$" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
 echo "**************"
 echo "*** Killing Adobe installer"
 echo "*** begin "
-for i in `ps auxwww | grep -i "Adobe Installer" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; kill -9 $i; done
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Adobe Installer" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Adobe Installer" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
+
 
 echo "**************"
 echo "*** Killing Adobe Update Daemon"
 echo "*** begin "
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "AdobeUpdateDaemon" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
 for i in `ps auxwww | grep -i "AdobeUpdateDaemon" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
+echo "*** end "
+echo "**************"
+
+echo "**************"
+echo "*** Killing Acrobat Update Helper"
+echo "*** begin "
+echo "Soft Kill"
+for i in `ps auxwww | grep -i "Acrobat Update Helper" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill $i; done
+sleep 1
+echo "Hard Kill"
+for i in `ps auxwww | grep -i "Acrobat Update Helper" | grep -v grep | tr -s " " | cut -d' ' -f2`; do echo "   " $i; sudo kill -9 $i; done
 echo "*** end "
 echo "**************"
 
